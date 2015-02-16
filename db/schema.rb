76ba278d873e7b8507009f47fec9f94d5ebb2ab6
nelson <nelson.lin@pushme.com.tw>
2015-02-16 22:13:42 +0800

    Convert rating to starts

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150213033705) do
+ActiveRecord::Schema.define(version: 20150216075642) do
 
   create_table "movies", force: :cascade do |t|
     t.string   "title"
@@ -23,6 +23,15 @@ ActiveRecord::Schema.define(version: 20150213033705) do
     t.datetime "updated_at",   null: false
     t.integer  "user_id"
     t.string   "image"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.integer  "rating"
+    t.text     "comment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer  "user_id"
+    t.integer  "movie_id"
   end
 
   create_table "users", force: :cascade do |t|
