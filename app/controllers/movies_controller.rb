ea365b4b67dc452a1ef87ac2ad352c1656cd46b5
nelson <nelson.lin@pushme.com.tw>
2015-02-16 22:13:42 +0800

    Convert rating to starts

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -10,6 +10,13 @@ class MoviesController < ApplicationController
   # GET /movies/1
   # GET /movies/1.json
   def show
+    @reviews = Review.where(movie_id: @movie.id).order('created_at DESC')
+
+    if @reviews.blank?
+      @avg_rating = 0
+    else
+      @avg_rating = @reviews.average(:rating).round(2)
+    end
   end
 
   # GET /movies/new
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,5 @@
 class Movie < ActiveRecord::Base
   belongs_to :user
+  has_many :reviews
   mount_uploader :image, ImageUploader
 end
